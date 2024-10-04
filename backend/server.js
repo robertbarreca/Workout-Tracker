c8538842b04f2e8cbfa71c84f55f08644d0c4364
Robert Barreca <barreca@wisc.edu>
2024-10-04 16:31:43 -0500

    connected to mongodb, made schema for workouts, and made POST request

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require("express")
 const workoutRoutes = require("./routes/workouts")
+const mongoose = require("mongoose")
 
 // creates express app
 const app = express()
@@ -15,7 +16,14 @@ app.use((req, res, next) => {
 // routes
 app.use("/api/workouts", workoutRoutes)
 
-// listen for a port
-app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}`)
-})
+// connect to DB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for a port only when connected to db
+        app.listen(process.env.PORT, () => {
+        console.log("connected to db and listening on port",  process.env.PORT)
+})    
+     })
+    .catch((error) => {
+        console.log(error)
+    })
