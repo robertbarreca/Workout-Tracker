ac36ba58af234e75c0e4c8e7fcde7a887d2647d0
Robert Barreca <barreca@wisc.edu>
2024-10-11 12:01:17 -0500

    debugging

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -15,7 +15,7 @@ export const useLogin = () => {
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({email, password})
         })
-        
+        console.log(res)
         const json = await res.json()
         if (!res.ok) {
             setIsLoading(false)
